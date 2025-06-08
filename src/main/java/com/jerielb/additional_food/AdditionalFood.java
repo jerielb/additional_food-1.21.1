c5f16d5c72e1b43c2e2c164d15681774b3e874f6
jerielB <jerielbongco98@gmail.com>
2025-06-08 22:12:32 +1000

    Initial commit

---
+++ src/main/java/com/jerielb/additional_food/AdditionalFood.java
@@ -0,0 +1,16 @@
+package com.jerielb.additional_food;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdditionalFood implements ModInitializer {
+	public static final String MOD_ID = "additional_food";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/com/jerielb/additional_food/AdditionalFoodClient.java
@@ -0,0 +1,10 @@
+package com.jerielb.additional_food;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdditionalFoodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
